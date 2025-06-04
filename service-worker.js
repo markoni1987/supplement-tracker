6444372eeb30beb97bbbb65161b044902e37a7d0
markoni1987 <marko.t@bluewin.ch>
2025-06-04 06:16:37 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,38 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("supplement-tracker").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/script.js",
-        "/manifest.json",
-        "/icon.png"
-      ]);
+const CACHE_NAME = "supplement-tracker-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/script.js",
+  "/manifest.json",
+  "/icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
   );
 });
